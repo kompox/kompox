ed9782ef81bb2e0f64e5e945fd1337b18486489b
YAEGASHI Takeshi <yaegashi@users.noreply.github.com>
2025-08-11 17:40:06 +0000

    chore(provider): remove legacy cluster/providers directory (migrated to provider/drivers)

--- cluster/providers/aks/aks.go
+++
@@ -1,3 +0,0 @@
-package aks
-
-// This package has been migrated. Use github.com/yaegashi/kompoxops/provider/drivers/aks.
--- cluster/providers/k3s/k3s.go
+++
@@ -1,3 +0,0 @@
-package k3s
-
-// This package has been migrated. Use github.com/yaegashi/kompoxops/provider/drivers/k3s.
